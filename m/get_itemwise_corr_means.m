0b15722a6ba910d35376b28e020525972e655b10
Chris Cox <chriscox@lsu.edu>
2022-10-05 16:14:03 -0500

    MAJOR REVISION * Look for variable `category` in table (labels encoded as strings). * Adapted to handle an arbitrary number of categories. Categories should be mutually exclusive for subsequent analyses to make sense but code does not check.

--- m/get_itemwise_corr_means.m
+++ m/get_itemwise_corr_means.m
@@ -8,15 +8,15 @@ function itemwise_corr_means = get_itemwise_corr_means(itemwise)
 
     func = @(x) varfun(@mean, x, ...
         'InputVariables', ["corr_all", "corr_within", "corr_between"], ...
-        'GroupingVariables', "is_animate");
+        'GroupingVariables', "category");
     tmp = cellfun(func, itemwise, 'UniformOutput', false);
     tbl = cat(1, tmp{:});
-    tbl_ani = removevars(tbl(tbl.is_animate, :), ["is_animate", "GroupCount"]);
-    tbl_ina = removevars(tbl(~tbl.is_animate, :), ["is_animate", "GroupCount"]);
-    tbl_ani.Properties.VariableNames = ...
-        ["corr_all_ani", "corr_within_ani", "corr_between_ani"];
-    tbl_ina.Properties.VariableNames = ...
-        ["corr_all_ina", "corr_within_ina", "corr_between_ina"];
-
-    itemwise_corr_means = [tbl_all, tbl_ani, tbl_ina];
+    category_labels = unique(tbl.category);
+    k = length(category_labels);
+    cell_tbl_cats = cell(1, k);
+    for i = 1:k
+        cell_tbl_cats{i} = removevars(tbl(tbl.category == category_labels(i), :), ["category", "GroupCount"]);
+        cell_tbl_cats{i}.Properties.VariableNames = ["corr_all_", "corr_within_", "corr_between_"] + category_labels(i);
+    end
+    itemwise_corr_means = cat(2, tbl_all, cell_tbl_cats{:});
 end
