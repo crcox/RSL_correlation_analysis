a8e3bf79faf9849517282a3af30398acc006457d
Chris Cox <chriscox@lsu.edu>
2023-01-18 10:07:08 -0600

    whitespace

--- m/write_corr3D.m
+++ m/write_corr3D.m
@@ -1,6 +1,14 @@
 function write_corr3D(results, filename)
     x = results.Properties.VariableNames;
-    vars = [x(contains(x, "Window")), x(matches(x, "target_label")), x(matches(x, "sim_source")), x(matches(x, "sim_metric")), "subject", "RandomSeed", x(contains(x, "corr3D"))];
+    vars = [
+      x(contains(x, "Window"))
+      x(matches(x, "target_label"))
+      x(matches(x, "sim_source"))
+      x(matches(x, "sim_metric"))
+      "subject"
+      "RandomSeed"
+      x(contains(x, "corr3D"))
+    ];
     tbl = results(:, vars);
     vars = replace(vars, "WindowStart", "window_start");
     vars = replace(vars, "WindowSize", "window_size");
