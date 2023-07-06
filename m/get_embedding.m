5b45b2b6452192bc6b9f5ff62b49203e6186f749
slfrisby <saskia.frisby@mrc-cbu.cam.ac.uk>
2023-07-06 17:28:23 +0100

    Added conditional support for embedding types

--- m/get_embedding.m
+++ m/get_embedding.m
@@ -3,27 +3,32 @@ function embedding = get_embedding(metadata, subject, target_label, target_type,
     m = select_by_field(metadata, struct('subject', subject));
     t = select_by_field(m.targets, struct( ...
                         'label', target_label, ...
-                        'type', "similarity", ...
+                        'type', target_type, ...
                         'sim_source', regexprep(sim_source, '_Dim[1-9]+', ''), ...
                         'sim_metric', sim_metric));
-
-    if isempty(filters_be)
-        z0 = true(m.nrow, 1);
-    else
-        z0 = get_row_filter(m.filters, filters_be);
-    end
-
-    [svecs, svals] = embed_similarity_matrix(t.target(z0, z0), 3);
-    if scale_singular_vectors
-        embedding = rescale_embedding(svecs, svals);
-    else
-        embedding = svecs;
-    end
-
     if isempty(filters)
         z1 = true(m.nrow, 1);
     else
         z1 = get_row_filter(m.filters, filters);
+    end
+                  
+    switch target_type
+        case "embedding"
+            embedding = t.target;
+            
+        case "similarity"
+            if isempty(filters_be)
+                z0 = true(m.nrow, 1);
+            else
+                z0 = get_row_filter(m.filters, filters_be);
+            end
+            
+            [svecs, svals] = embed_similarity_matrix(t.target(z0, z0), 3);
+            if scale_singular_vectors
+                embedding = rescale_embedding(svecs, svals);
+            else
+                embedding = svecs;
+            end
     end
 
     embedding = embedding(z1, :);
